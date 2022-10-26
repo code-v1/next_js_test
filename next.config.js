31b2f65e19a0dc9d24d0495e18a9893702fc264b
code-v1 <tovereric@gmail.com>
2022-10-25 22:44:17 -0400

    mod export next config

--- next.config.js
+++ next.config.js
@@ -2,21 +2,31 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  build: {
-    extend(config, {}) {
-        config.node = {
-            fs: 'empty'
+  
+  
+  
+}
+
+
+
+
+
+module.exports = (phase, { defaultConfig }) => {
+  return {
+    ...defaultConfig,
+
+    webpack: (config) => {
+      config.resolve = {
+        ...config.resolve,
+        fallback: {
+          "fs": false,
+          "path": false,
+          "os": false,
         }
-    }
+      }
+      return config
+    },
+  }
 }
-  
-  
-}
-
-
-
-
-
-
 
 module.exports = nextConfig
