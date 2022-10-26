ddd26d3c20b66c7cedbddb145e871cca3f4f7604
code-v1 <tovereric@gmail.com>
2022-10-25 22:23:59 -0400

    nextconfi webpack changes

--- next.config.js
+++ next.config.js
@@ -2,30 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  fs: false,
+  path: false
+  
   
 }
 
 
-module.exports = {
-  webpack: (config, { isServer }) => {
-      if (!isServer) {
-          // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
-          config.resolve.fallback = {
-              fs: false
-          }
-      }
 
-      return config;
-  }
-}
 
-module.exports = {
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
-}
 
 module.exports = {
   future: {
