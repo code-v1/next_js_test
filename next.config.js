b3fdf765d4d810e7161caffdbf43df9cd63d25fa
code-v1 <tovereric@gmail.com>
2022-10-25 22:42:23 -0400

    nextconfig fs exclude

--- next.config.js
+++ next.config.js
@@ -2,7 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  
+  build: {
+    extend(config, {}) {
+        config.node = {
+            fs: 'empty'
+        }
+    }
+}
   
   
 }
@@ -11,20 +17,6 @@ const nextConfig = {
 
 
 
-module.exports = {
-  future: {
-    webpack5: true, // by default, if you customize webpack config, they switch back to version 4. 
-      // Looks like backward compatibility approach.
-  },
-  webpack(config) {
-    config.resolve.fallback = {
-      ...config.resolve.fallback, // if you miss it, all the other options in fallback, specified
-        // by next.js will be dropped. Doesn't make much sense, but how it is
-      fs: false, // the solution
-    };
 
-    return config;
-  },
-};
 
 module.exports = nextConfig
