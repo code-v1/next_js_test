895a6937b7eb267ec265f926ada230c87528ab08
code-v1 <tovereric@gmail.com>
2022-10-27 20:44:13 -0400

    removed date complete

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Layout, { siteTitle } from './components/layout';
 import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../lib/posts';
 import Link from 'next/link';
-import Date from './posts/date';
+
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
